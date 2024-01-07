77f6f33c797683e6c7825c5b2664f4241fe09f9e
nonipas <chinonsopascal@gmail.com>
2024-01-08 00:29:47 +0100

    latest update

--- resources/views/email/password_reset.blade.php
+++ resources/views/email/password_reset.blade.php
@@ -28,7 +28,7 @@
                                                 <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                                     <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                                                         <a href="{{url('/')}}" style="display: block; margin: 0 auto; text-align: center;">
-                                                            <img src="{{url('/')}}/assets/images/logo/{{App\Helpers\Helpers::getConfig('logo')??'logo-dark.svg'}}" alt="E-thrift" style="width: auto; height: 70px; object-fit: cover;">
+                                                            <img src="{{asset('assets/images/logo').'/'.(App\Helpers\Helpers::getConfig('logo_light') ?? 'logo-light.svg')}}" alt="E-thrift" style="width: auto; height: 70px; object-fit: cover;">
                                                         </a>
                                                         <br><br>
                                                         <img src="{{url('/')}}/assets/images/icon-sec.png" alt="sec" style="display: block; margin: 0 auto; width: 32px; height: 32px; object-fit: cover;">
@@ -43,7 +43,7 @@
                                                 </tr>
                                                 <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                                     <td class="content-block" itemprop="handler" itemscope itemtype="http://schema.org/HttpActionHandler" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-                                                        <a href="{{$data['url']}}" itemprop="url" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #34c38f; margin: 0; border-color: #34c38f; border-style: solid; border-width: 8px 16px;">
+                                                        <a href="{{$data['url']}}" itemprop="url" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #34c38f; margin: 0 auto; border-color: #34c38f; border-style: solid; border-width: 8px 16px;">
                                                             click here to reset your password
                                                         </a>
                                                     </td>
